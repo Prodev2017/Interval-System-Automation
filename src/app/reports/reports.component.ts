503f2259843ec08a2902b85feac031c9c37dab70
Prodev2017 <chris.bergman320@gmail.com>
2017-06-25 03:03:08 -0500

    change angular 2

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
+declare var $: any;
+declare var Pleasure: any;
+declare var Layout: any;
+declare var FormsSwitch: any;
+declare var FormsSwitchery: any;
 @Component({
   selector: 'app-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.css']
+  templateUrl: './reports.component.html'
 })
 export class ReportsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+		Pleasure.init();
+   	Layout.init();
+		FormsSwitch.init();
+		FormsSwitchery.init();
+	});
   }
 
 }
